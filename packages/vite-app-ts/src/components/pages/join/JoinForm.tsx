a5adc81bfcd391eacca40c9eee640d12feea8cac
Igor Line <igor@line.ee>
2022-04-10 01:50:11 +0200

    fix(frontend): update upload to w3storage

--- packages/vite-app-ts/src/components/pages/join/JoinForm.tsx
+++ packages/vite-app-ts/src/components/pages/join/JoinForm.tsx
@@ -1,16 +1,42 @@
 import { Form, Button } from 'antd';
 import { FC } from 'react';
-
-import storeFiles from '../../../../scripts/store.mjs'
+import { Web3Storage } from 'web3.storage';
+import { CIDString } from 'web3.storage/dist/src/lib/interface';
 
 import CoverLetterForm from './CoverLetterForm';
 
 import SubmitPageHeader from '~~/components/common/SubmitPageHeader';
 
+const makeFileObjects = (content: string): File[] => {
+  const blob = new Blob([content], { type: 'text/plain' });
+
+  const files = [new File([blob], 'cover-letter.txt')];
+  return files;
+};
+
+const makeStorageClient = (): Web3Storage | void => {
+  if (process.env.VITE_WEB3_STORAGE_TOKEN) {
+    return new Web3Storage({
+      token: process.env.VITE_WEB3_STORAGE_TOKEN,
+    });
+  }
+};
+
+async function storeFiles(files: File[]): Promise<CIDString | undefined> {
+  const client = makeStorageClient();
+  const cid = await client?.put(files);
+  console.log('stored files with cid:', cid);
+  return cid;
+}
+
+interface Values {
+  'cover-letter': string;
+}
+
 const JoinForm: FC<any> = () => {
-  const submit = (values: any): void => {
-    storeFiles(CoverLetterForm)
-    // TODO: upload `values["cover-letter"] to web3.storage
+  const submit = async (values: Values): Promise<void> => {
+    const files = makeFileObjects(values['cover-letter']);
+    const cid = await storeFiles(files);
   };
   return (
     <div className="join-form">
