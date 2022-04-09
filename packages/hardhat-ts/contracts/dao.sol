69acdcd735a75c9c00805eeb12e43c44ffa08003
0x00badcode <paul.crinquand@epitech.eu>
2022-04-09 20:18:57 +0200

    teal : fixed the noted problems, missing the ceramic link

--- packages/hardhat-ts/contracts/dao.sol
+++ packages/hardhat-ts/contracts/dao.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CryptoDevsDAO is IERC721Receiver {
+contract licensedao {
     uint DAOmembers;
 
     constructor() {
         owner = msg.sender; // setting the owner the contract deployer
         DAOmembers = 0;
+        members[owner] = true;
     }
 
     enum proposalStatus {
+        NONE,
         ACCEPTED,
         REJECTED
     }
@@ -28,115 +30,109 @@ contract CryptoDevsDAO is IERC721Receiver {
     }
 
     struct Proposal {
-        uint proposalID;
         address proposed;
         uint256 deadline;
         uint256 votesFor;
         uint256 votesAgainst;
         ProposalType proposalType;
         bool executed;
-        uint voterNumber;
+        proposalStatus status;
         mapping(address => bool) voters;
+        mapping(address => voteType) _voteType;
     }
     
     // Map Proposal ID to Proposal
     mapping(uint256 => Proposal) public proposals;
-    mapping(address => bool) public memberAddresses;
+    mapping(address => bool) public members;
+    mapping(address => uint) public coverLetters;
 
-    event newUserProposed(address userProposed);
-    event newLicenseProposed(address licenseProposed);
-    event userJoined(address newUserAddress);
-    event userRejected(address rejectedUserAddress);
-    event newLicenseValidated(address newLicenseAddress);
+    event NewUserProposed(address userProposed);
+    event NewLicenseProposed(address licenseProposed);
+    event UserJoined(address newUserAddress);
+    event UserRejected(address rejectedUserAddress);
+    event NewLicenseValidated(address newLicenseAddress);
     event LicenseRejected(address rejectedLicenseAddress);
 
-    memberAddresses[owner] = true;
+    uint chorum = DAOmembers * 10000 / 3000;
 
     uint256 public numProposals;
 
     modifier memberOnly() {
-        require(members[msg.sender].lockedUpNFTs.length > 0, "NOT_A_MEMBER");
+        require(members[msg.sender] == 0, "You need to be a member");
         _;
     }
 
     function newProposal(address _proposed, ProposalType _proposalType) external memberOnly returns (uint256){
-        if (_proposalType == ProposalType.MEMBER) {
-            require(memberAddresses[_proposed] == 0, "USER IS ALREADY IN THE DAO");
-            proposals.proposalID = proposalIDGeneral
-            proposals.proposed = _proposed;
-            proposals.deadline = now + 7 days;
-            proposals.proposalType = MEMBER;
-            emit newUserProposed(_proposed);
-        } else if (_proposalType == ProposalType.LICENSE) {
-            proposals.proposalID = proposalIDGeneral
-            proposals.proposed = _proposed;
-            proposals.deadline = now + 7 days;
-            proposals.proposalType = LICENSE;
-            emit newLicenseProposed(_proposed);
-        }
-        ///this block updates proposal number of the contract and returns current proposal to front
         Proposal storage proposal = proposals[numProposals];
+        if (_proposalType == ProposalType.MEMBER) {
+            require(members[_proposed] == 0, "USER IS ALREADY IN THE DAO");
+            proposals[proposalType] = MEMBER;
+            emit NewUserProposed(_proposed);
+        } else if (_proposalType == ProposalType.LICENSE) {
+            proposals[proposalType] = LICENSE;
+            emit NewLicenseProposed(_proposed);
+        }
+        proposals[proposed] = _proposed;
+        proposals[deadline] = now + 7 days;
+        ///this block updates proposal number of the contract and returns current proposal to front
         numProposals++;
         return numProposals - 1;
     }
 
-
-    // Vote yes/no on a given proposal
+    /// Vote yes/no on a given proposal
     function voteOnProposal(uint256 _proposalId, VoteType _vote) external memberOnly {
         Proposal storage proposal = proposals[_proposalId];
         require(proposal.deadline > block.timestamp, "INACTIVE_PROPOSAL");
-        require(proposal.voters[msg.sender] == false, "ALREADY_VOTED");
-
-        proposal.voters[msg.sender] = true;
-        proposal.voterNumber += 1;
-
-        if (_vote == VoteType.voteFor) {
-            proposal.voteFor += 1;
+        if (proposal.voters[msg.sender] == false) {
+            if (proposal[msg.sender].voteType == FOR && _vote == voteType.AGAINST) {
+                proposal.voteFor -= 1;
+                proposal.voteAgainst += 1;
+            } else if (proposal[msg.sender].voteType == AGAINST && _vote == voteType.FOR) {
+                proposal.voteFor += 1;
+                proposal.voteAgainst -= 1;
+            }            
         } else {
-            proposal.voteAgainst += 1;
+            proposal.voters[msg.sender] = true;
+
+            if (_vote == VoteType.voteFor) {
+                proposal.voteFor += 1;
+            } else {
+                proposal.voteAgainst += 1;
+            }
         }
     }
 
-
-    // Execute a proposal
+    /// Execute a proposal
     function executeProposal(uint256 _proposalId) external memberOnly {
         Proposal storage proposal = proposals[_proposalId];
+        
+        if (proposal.deadline <= block.timestamp && (proposal.votesFor + proposal.voteAgainst) < chorum) {
+            proposal.status = REJECTED;
+            revert();
+        }
+
         require(proposal.deadline <= block.timestamp, "ACTIVE_PROPOSAL");
         require(proposal.executed == false, "ALREADY_EXECUTED");
-        require(proposal.voterNumber == DAOmembers*0.30);
+        require((proposal.votesFor + proposal.voteAgainst) >= chorum);
         proposal.executed = true;
 
-        if (proposal.voteFor > (proposal.voterNumber/2)) {
+        if (proposal.voteFor > ((proposal.votesFor + proposal.votesAgainst)/2)) {
             if (proposal.proposalType == ProposalType.MEMBER) {
                 addUser(proposal.address);
             } else {
                 addLicense(proposal.address);
             }
+            proposal.status = ACCEPTED;
         }
     }
 
-    function isUser() public returns (bool) {
-        bool isInDao;
-        if (memberAddresses[msg.sender] == 0) {
-            isInDao = 1;
-        } else {
-            isInDao = 0;
-        }
-        return isInDao;
-    }
-
-    function addUser(address newUserAddress) public {
-        memberAddresses[_addressToMemberlist] = true;
+    function addUser(address newUserAddress) internal {
+        members[_addressToMemberlist] = true;
         DAOmembers += 1;
-        emit userJoined(newUserAddress);
+        emit UserJoined(newUserAddress);
     }
 
-    function addLicense(address newLicense) public {
-
-    // function to quit the DAO
-    function quit() external memberOnly {
-        Member storage member = members[msg.sender];
-        require(block.timestamp - member.joinedAt > 5 minutes, "MIN_MEMBERSHIP_PERIOD" );
-        delete members[msg.sender];
+    function addLicense(address newLicense) internal {
+        ///we need to decide what to do
     }
 }
