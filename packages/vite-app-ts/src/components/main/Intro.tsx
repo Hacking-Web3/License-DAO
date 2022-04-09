d06c4be7228f89c3894239c94be7314fc05962dd
Igor Line <igor@line.ee>
2022-04-09 14:00:21 +0200

    feat(frontend): add intro block

---
+++ packages/vite-app-ts/src/components/main/Intro.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Intro: FC<any> = () => (
+  <div className="intro">
+    <div className="intro-text">Some very welcome text 3 lines welcome text and something about?</div>
+    <div className="intro-image">
+      <img src="/assets/intro-image.svg" alt="" />
+    </div>
+  </div>
+);
+
+export default Intro;
