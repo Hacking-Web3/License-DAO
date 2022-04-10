c81c466dae1236c642b7739e83bccfe42b0b9ecf
leon <leon.ducasse@epitech.eu>
2022-04-10 04:30:35 +0200

    [ADD] graph config fix

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,21 +1,21 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 import {
-  LicenceDAO,
+  LicenseDAO,
   NewUserProposed
-} from "../generated/LicenceDAO/LicenceDAO";
+} from "../generated/LicenseDAO/LicenseDAO";
 import { MemberProposal } from "../generated/schema";
 
-export function handleMemberProposal(event: NewUserProposed): void {
+export function handleNewUserProposed(event: NewUserProposed): void {
   let userAddress = event.params.userAddress.toHexString();
 
-  let memberProposal = MemberProposal.load(userAddress + event.transaction.hash);
+  let memberProposal = MemberProposal.load(userAddress + "-" + event.transaction.hash.toHex());
 
   if (memberProposal == null) {
-    memberProposal = new MemberProposal(userAddress.toHex() + "-" + event.transaction.hash.toHex());
+    memberProposal = new MemberProposal(userAddress + "-" + event.transaction.hash.toHex());
     memberProposal.address = event.params.userAddress;
     memberProposal.ipfsHash = event.params.ipfsHash;
     memberProposal.createdAt = event.block.timestamp;
-    memberProposal.transactionHash = event.transaction.hash;
+    /* memberProposal.transactionHash = event.transaction.hash; */
     memberProposal.save();
   }
 }
